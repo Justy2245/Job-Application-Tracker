290112db11b0117f1e5ec458ead71292ce2cb691
Justin Takayoshi <takayoshi2245@gmail.com>
2023-09-01 22:11:09 -0700

    Added functionality for add button

--- App-Tracker/src/Components/Input.jsx
+++ App-Tracker/src/Components/Input.jsx
@@ -3,6 +3,19 @@ import React, {useState} from "react";
 const Input = ({ app }) => {
     const [apps, setApps] = useState(app);
 
+    const addApp = async event => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://localhost:5000/apps', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(apps)
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
     return (
         <>
         <div>
@@ -18,13 +31,13 @@ const Input = ({ app }) => {
                     <h4 className="modal-title">Add New Application</h4>
                 </div>
                 <form className="modal-body">
-                    <input type="text" className="form-control" placeholder ='Enter Job Title' name = 'name' />
-                    <input type="text" className="form-control mt-2 mb-2" placeholder ='Enter Company Name' />
-                    <input type="text" className="form-control" placeholder ='Enter Location' />
-                    <input type="text" className="form-control mt-2 mb-2" placeholder ='Enter Date Applied' />
+                    <input type="text" className="form-control" placeholder ='Enter Job Title' name = 'title' onChange ={event => setApps({...app, [event.target.name]: event.target.value})}/>
+                    <input type="text" className="form-control mt-2 mb-2" placeholder ='Enter Company Name' name = 'company' onChange ={event => setApps({...app, [event.target.name]: event.target.value})}/>
+                    <input type="text" className="form-control" placeholder ='Enter Location' name = 'location' onChange ={event => setApps({...app, [event.target.name]: event.target.value})}/>
+                    <input type="text" className="form-control mt-2 mb-2" placeholder ='Enter Date Applied' name = 'date_applied' onChange ={event => setApps({...app, [event.target.name]: event.target.value})}/>
                 </form>
                 <div className="modal-footer">
-                    <button type="button" className="btn btn-warning" data-bs-dismiss="modal">
+                    <button type="button" className="btn btn-warning" data-bs-dismiss="modal" onClick = {event => addApp(event)}>
                         Add
                     </button>
                     <button type="button" className="btn btn-danger" data-bs-dismiss="modal">
