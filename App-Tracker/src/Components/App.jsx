fdd55ccd83a089bd36178870468b62f010894529
Justin Takayoshi <takayoshi2245@gmail.com>
2023-09-01 18:31:18 -0700

    Seting up frontend

---
+++ App-Tracker/src/Components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='header'>
+        <h1>Job Application Tracker</h1>
+        <input type="text" Test/>
+        <button>Search</button>
+      </div>
+    </>
+  )
+}
+
+export default App
