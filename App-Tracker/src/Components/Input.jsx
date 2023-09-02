0d323f6ddf3b7201bfb3c3cc4d0c41984d854316
Justin Takayoshi <takayoshi2245@gmail.com>
2023-09-01 21:51:21 -0700

    Added bootstrap and modal for add button

--- App-Tracker/src/Components/Input.jsx
+++ App-Tracker/src/Components/Input.jsx
@@ -6,7 +6,33 @@ const Input = ({ app }) => {
     return (
         <>
         <div>
-            <button type = "button"> Add Application</button>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target='#mainModal'>
+                Add New App
+            </button>
+        </div>
+
+        <div className="modal" id ='mainModal'>
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h4 className="modal-title">Add New Application</h4>
+                </div>
+                <form className="modal-body">
+                    <input type="text" className="form-control" placeholder ='Enter Job Title' name = 'name' />
+                    <input type="text" className="form-control mt-2 mb-2" placeholder ='Enter Company Name' />
+                    <input type="text" className="form-control" placeholder ='Enter Location' />
+                    <input type="text" className="form-control mt-2 mb-2" placeholder ='Enter Date Applied' />
+                </form>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-warning" data-bs-dismiss="modal">
+                        Add
+                    </button>
+                    <button type="button" className="btn btn-danger" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                </div>
+                </div>
+            </div>
         </div>
         </>
     )
--- App-Tracker/vite.config.js
+++ App-Tracker/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+/*// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 8080,
+    hot: true
+  }
+}
